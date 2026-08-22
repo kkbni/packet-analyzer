4df6b08e98e4706b0dcdcf37cc4abedf098947ef
Mykola <motsamyk@cvut.cz>
2026-08-22 20:15:06 +0200

    Changed the naming according to convention

--- src/analyzer/parsers/ethernet.py
+++ src/analyzer/parsers/ethernet.py
@@ -2,25 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class EthernetFrame:
-    dest_addr: bytes
-    src_addr: bytes
-    type: int
+    dest_mac: str
+    src_mac: str
+    ether_type: int   # Network Layer Protocol
     payload: bytes
 
     @classmethod
     def parse(cls, data: bytes):
         # parse raw bytes and return an EthernetFrame object
 
-        dest = data[0:6]
-        src = data[6:12]
-        type = int.from_bytes(data[12:14], 'big')
+        dest_mac = data[0:6].hex(':')
+        src_mac = data[6:12].hex(':')
+        ether_type = int.from_bytes(data[12:14], 'big')
         payload = data[14:]
 
-        return cls(dest, src, type, payload)
+        return cls(dest_mac, src_mac, ether_type, payload)
 
     def __str__(self):
-        dest_str = self.dest_addr.hex(':')
-        src_str = self.src_addr.hex(':')
-        type = self.type
-
-        return f'src: {src_str}\ndest: {dest_str}\ntype: {type:#06x}\n'
+        return f'src MAC: {self.src_mac}\ndest MAC: {self.dest_mac}\nprotocol: {self.ether_type:#06x}\n'
