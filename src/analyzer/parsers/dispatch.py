ef650733b51c7fb2a867628f9a3e8ce8534f9f27
Mykola <motsamyk@cvut.cz>
2026-08-24 17:41:20 +0200

    Setting up Transport Layer parsing

--- src/analyzer/parsers/dispatch.py
+++ src/analyzer/parsers/dispatch.py
@@ -1,14 +1,27 @@
 from .network import IPv4Packet, ARPPacket, IPv6Packet
 
+# ---- PROTO CONSTANTS ----
+
 ETHER_TYPE_IPV4 = 0x0800
 ETHER_TYPE_ARP = 0x0806
 ETHER_TYPE_IPV6 = 0x86DD
+
+IP_PROTO_TCP = 6
+IP_PROTO_UDP = 17
+
+# ---- DISPATCH MAPS ----
 
 PARSERS_NETWORK = {
     ETHER_TYPE_IPV4 : IPv4Packet.parse,
     ETHER_TYPE_ARP : ARPPacket.parse,
     ETHER_TYPE_IPV6 : IPv6Packet.parse
 }
+
+PARSERS_TRANSPORT = {
+
+}
+
+# ---- DISPATCH FUNCTIONS ----
 
 def parse_network_layer(ether_type: int, data: bytes):
 
@@ -28,3 +41,6 @@ def parse_network_layer(ether_type: int, data: bytes):
     else:
         print(f'Uknown Network Layer protocol: {ether_type:#06x}\n')
         return None
+
+def parse_transport_layer(ip_proto: int, data: bytes):
+    pass
