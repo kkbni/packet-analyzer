7dd80edbd5f6e3d5aded69659bee8672a6b0d97b
Mykola <motsamyk@cvut.cz>
2026-09-21 21:07:13 +0200

    Add command-line argument for interface selection and improve error handling

--- src/analyzer/__main__.py
+++ src/analyzer/__main__.py
@@ -1,15 +1,41 @@
+import argparse
+import sys
 import queue
 import threading
 
 from .capture import PacketCapturer, capture_worker
 from .ui import start_ui
 
+# error codes
+ENODEV = 19
+EPERM = 1
+
 def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        '-i', '--interface',
+        type=str,
+        default='eth0',
+    )
+    args = arg_parser.parse_args()
+
+    # try to bind the interface
+    try:
+        # create a capturer
+        capturer = PacketCapturer(args.interface)
+    except OSError as e:
+        if e.errno == ENODEV:
+            print(f'Error: interface \'{args.interface}\' doesn\'t exist.')
+            print('Check available interfaces with "ifconfig".\n')
+        elif e.errno == EPERM:
+            print('Error: operation not permitted.')
+            print('Raw sockets require elevated privileges. Did you forget \'sudo\'?\n')
+        else:
+            print(f'Error while binding to \'{args.interface}\': {e}\n')
+        sys.exit(1)
+
     packet_history = {}
     packet_queue = queue.Queue()
-
-    # create a capturer
-    capturer = PacketCapturer('eth0')
 
     # start the packet capture in a background thread
     worker = threading.Thread(target=capture_worker, args=[capturer, packet_history, packet_queue], daemon=True)
